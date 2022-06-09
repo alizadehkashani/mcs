fac3fdd68d7a51fe78b1775863658c1262a1056d
Alizadeh Kashani <ramin.Alizadeh-Kashani@snp-ag.com>
2022-06-09 20:32:21 +0200

    additional work on user maintaince

--- lib/admin/deleteuser.php
+++ lib/admin/deleteuser.php
@@ -2,6 +2,33 @@
 
 	include('../dbconfig.php');
 
+	$reply = [];
+
+	//check if user with that name is available
+	$sql = $dbconnection->prepare("SELECT COUNT(username) FROM user WHERE username = :username");
+	$sql->bindParam(':username', $_POST["username"]);
+	$sql->execute();
+	$result = $sql->fetch(PDO::FETCH_NUM);
 	
+	//if no user with that name exists
+	if($result[0] == 0){
+		$reply['result'] = 1;
+		$reply['message'] = "User does not exist";
+	}else if($result[0] == 1){
+		//if user with name exist, delete user
+		$sql = $dbconnection->prepare("DELETE FROM user WHERE username = :username");
+		$sql->bindParam(':username', $_POST["username"]);
+		$sql->execute();
+
+		$reply['result'] = 0;
+		$reply['message'] = "User deleted";
+
+	}else{
+		//if more than one user exist, database error
+		$reply['result'] = 1;
+		$reply['message'] = "Database error";
+	}
+
+	echo(json_encode($reply));
 
 ?>
