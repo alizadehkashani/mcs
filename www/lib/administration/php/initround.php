6a1e3a08ec41ac3ad90270c6e2a9092f3dacfb58
Ramin Tim Alizadeh Kashani <ramin.alizadeh.kashani@googlemail.com>
2023-03-03 14:56:05 +0100

    #12 fixed bug

--- www/lib/administration/php/initround.php
+++ www/lib/administration/php/initround.php
@@ -6,9 +6,25 @@
 
 	$result = [];
 
+	$zero = 0;
 	$one = 1;
 
+	//reset all current groups of round
+	$query = "
+		UPDATE groups
+		SET currentgroup = :zero
+		WHERE tid = :tid
+		AND mdnumber = :mdnumber
+		AND rnumber = :rnumber
+		";
 
+	$sql = $dbconnection->prepare($query);
+	$sql->bindParam(":zero", $zero);
+	$sql->bindParam(":tid", $data["tid"]);
+	$sql->bindParam(":mdnumber", $data["mdnumber"]);
+	$sql->bindParam(":rnumber", $data["rnumber"]);
+	$sql->execute();
+		
 	//get tracks of tournament
 	$query = "
 		SELECT trackid
@@ -19,9 +35,11 @@
 	$sql = $dbconnection->prepare($query);
 	$sql->bindParam(":tid", $data["tid"]);
 	$sql->execute();
-	$tracks = $sql->fetchAll(PDO::FETCH_ASSOC);
-	
+	$tracks = $sql->fetchAll(PDO::FETCH_ASSOC);	
 
+	print_r($tracks);
+
+	//loop through tracks and set first group to current group
 	for($i = 0; $i < count($tracks); $i++){
 
 		//get first round
@@ -36,6 +54,7 @@
 				(SELECT groupid
 					FROM groups
 					WHERE tid = :tid
+					AND trackid = :trackid
 					AND mdnumber = :mdnumber
 					AND rnumber = :rnumber 
 					ORDER BY grouporder ASC
@@ -57,6 +76,4 @@
 	$result["message"] = "Runden initialisiert";
 
 	echo(json_encode($result));
-
-
 ?>
