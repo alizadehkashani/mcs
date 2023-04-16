fe059344e57bbef59f6b9218066d771790a38136
Ramin Tim Alizadeh Kashani <ramin.alizadeh.kashani@googlemail.com>
2023-04-16 21:20:05 +0200

    #15

--- www/lib/administration/php/setrcurrent.php
+++ www/lib/administration/php/setrcurrent.php
@@ -52,6 +52,70 @@
 	$sql->bindParam(":rid", $input["rid"]);
 	$sql->bindValue(":rcurrent", 1);
 	$sql->execute();
+
+	//start initialize round
+	
+	//reset all current groups of round
+	$query = "
+		UPDATE groups
+		SET currentgroup = :zero
+		WHERE tid = :tid
+		AND mid = :mid
+		AND rid = :rid
+		";
+
+	$sql = $dbconnection->prepare($query);
+	$sql->bindValue(":zero", 0);
+	$sql->bindParam(":tid", $data["tid"]);
+	$sql->bindParam(":mid", $data["mid"]);
+	$sql->bindParam(":rid", $data["rid"]);
+	$sql->execute();
+		
+	//get tracks of tournament
+	$query = "
+		SELECT trackid
+		FROM tracks
+		WHERE tid = :tid
+		";
+
+	$sql = $dbconnection->prepare($query);
+	$sql->bindParam(":tid", $data["tid"]);
+	$sql->execute();
+	$tracks = $sql->fetchAll(PDO::FETCH_ASSOC);	
+
+	//loop through tracks and set first group to current group
+	for($i = 0; $i < count($tracks); $i++){
+
+		//get first round
+		$query = "
+			UPDATE groups 
+			SET currentgroup = :one
+			WHERE tid = :tid
+			AND mid = :mid
+			AND rid = :rid 
+			AND trackid = :trackid
+			AND groupid =
+				(SELECT groupid
+					FROM groups
+					WHERE tid = :tid
+					AND trackid = :trackid
+					AND mid = :mid
+					AND rid = :rid 
+					ORDER BY grouporder ASC
+					LIMIT 1)
+					
+			";
+
+		$sql = $dbconnection->prepare($query);
+		$sql->bindValue(":one", 1);
+		$sql->bindParam(":tid", $data["tid"]);
+		$sql->bindParam(":mid", $data["mid"]);
+		$sql->bindParam(":rid", $data["rid"]);
+		$sql->bindParam(":trackid", $tracks[$i]["trackid"]);
+		$sql->execute();
+		$result = $sql->fetch(PDO::FETCH_ASSOC);
+	}
+	//end initialize round
 	
 	$response["result"] = 0;
 	$response["message"] = "Runde aktiv";
