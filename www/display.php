a22fa4d81a43ec133cf0e8da73bdaf730929e6c7
Ramin Tim Alizadeh Kashani <ramin.alizadeh.kashani@googlemail.com>
2023-08-03 01:15:54 +0200

    removed php tag from display page

--- www/display.php
+++ www/display.php
@@ -1,11 +1,7 @@
-<php
-    
-?>
-
 <html>
     <head>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	<link rel="icon" type="image/x-icon" href="/lib/favicon.png">
+	<link rel="icon" type="image/x-icon" href="lib/favicon.png">
         <link rel="stylesheet" type="text/css" href="lib/mcs.css"></script>
         <link rel="stylesheet" type="text/css" href="lib/display/display.css"></script>
         <script src="lib/mcs.js" defer></script>
@@ -14,9 +10,9 @@
     </head>
     <body>
 	<div id = "displaymaincontainer">
-		<div id = "displayheadmaincontainer"></div>
-		<div id = "displaygroupsmaincontainer"></div>
-		<div id = "displayfootmaincontainer"></div>
+		<div id="displayheadmaincontainer"></div>
+		<div id="displaygroupsmaincontainer"></div>
+		<div id="displayfootmaincontainer"></div>
 	</div>
     </body>
 </html>
